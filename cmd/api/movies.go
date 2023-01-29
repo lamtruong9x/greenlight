af09ddb6701d4d6424dff9397db245890b31ccfc
truong <damlamtruong@gmail.com>
2023-01-29 18:16:26 +0700

    json error response

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -11,7 +11,7 @@ import (
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readIDParam(r)
 	if err != nil {
-		http.NotFound(w, r)
+		app.notFoundResponse(w, r)
 		return
 	}
 
@@ -29,8 +29,8 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 	// Encode the struct to JSON and send it as the HTTP response.
 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
 	if err != nil {
-		app.logger.Print(err)
-		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		app.serverErrorResponse(w, r, err)
+		return
 	}
 }
 
