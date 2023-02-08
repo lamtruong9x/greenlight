073b6c51c9deb5d394b598f8553db7f23260a663
truong <damlamtruong@gmail.com>
2023-02-08 17:04:24 +0700

    add db

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
@@ -10,6 +12,7 @@ import (
 
 	"github.com/go-playground/validator/v10"
 	myValidator "github.com/lamtruong9x/greenlight/internal/validator"
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -17,6 +20,9 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn string
+	}
 }
 
 type application struct {
@@ -32,6 +38,7 @@ func main() {
 	// Read the value of the port and env command-line flag into struct.
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://greenlight:pa55word@localhost/greenlight?sslmode=disable", "PostgreSQL DSN")
 
 	// Initialize a new logger which writes messages to the standard out stream,
 	// prefixed with the current date and time.
@@ -39,6 +46,12 @@ func main() {
 
 	// Initialize a new validator
 	val, err := myValidator.New()
+	if err != nil {
+		panic(err)
+	}
+
+	// Create a connection pool
+	_, err = openDB(cfg)
 	if err != nil {
 		panic(err)
 	}
@@ -65,4 +78,20 @@ func main() {
 	// Start the HTTP server
 	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
 	app.logger.Fatal(srv.ListenAndServe())
+}
+
+func openDB(cnf config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cnf.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Microsecond)
+	defer cancel()
+
+	if err = db.PingContext(ctx); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
